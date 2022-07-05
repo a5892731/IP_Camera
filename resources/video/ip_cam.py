4b7b134030d6e05d00847d54fe60f85915b56fda
jakubz <a5892731@o2.pl>
2022-07-05 12:15:47 +0200

    create project ()

--- resources/video/ip_cam.py
+++ resources/video/ip_cam.py
@@ -1,6 +1,11 @@
 # author: a5892731
+# creation date: 20220-07-05
+# last modification: 20220-07-05
+# version 1.0
+
 
 # source: https://stackoverflow.com/questions/49978705/access-ip-camera-in-python-opencv
+# source: https://stackoverflow.com/questions/71755653/set-a-custom-timeout-for-opencv-videocapture-read-function
 
 
 import cv2
@@ -8,32 +13,45 @@ import cv2
 
 
 
+class Ip_Camera():
 
-#class Ip_Camera():
-
-
-
-
-
-
-'''
-the ONVIF Device Manager program can be helpful in importing full url of ip camera
-'''
-url = 'rtsp://192.168.1.201:554/mpeg4?username=admin&password=E10ADC3949BA59ABBE56E057F20F883E'
+    def __init__(self):
+        '''
+        the ONVIF Device Manager program can be helpful in importing full url of ip camera
+        '''
+        self.url = 'rtsp://192.168.1.201:554/mpeg4?username=admin&password=E10ADC3949BA59ABBE56E057F20F883E'
+        self.connection = False
 
 
-cap = cv2.VideoCapture(url)
+    def connect_to_camera(self):
+        self.cap = cv2.VideoCapture(self.url)
+
+
+    def refresh(self):
+        '''
+        this function need to be in while loop
+        '''
+        ret, frame = self.cap.read()
+        cv2.imshow('frame', frame)
+        k = cv2.waitKey(1)
+
+
+    def disconnect(self):
+        '''
+        this function should be called in the end of program
+        '''
+        cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+
+    camera = Ip_Camera()
+
+
+    while True:
+        camera.refresh()
+
+    camera.disconect()
 
 
 
-
-while(True):
-    ret, frame = cap.read()
-    cv2.imshow('frame',frame)
-    print("Running...")
-    k = cv2.waitKey(1)
-
-
-
-
-cv2.destroyAllWindows()
